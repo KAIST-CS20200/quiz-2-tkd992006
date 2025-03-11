90be814a96f3edb917b8aafa55b04cf37ea98d6a
“tkd992006” <tkd992006@naver.com>
2025-03-11 09:50:24 +0900

    asdf

--- Math.fs
+++ Math.fs
@@ -5,5 +5,10 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
+let isEven n = n % 2 = 0
 let collatz n =
-  0
+  let rec helper n count =
+    if n = 1 then count
+    elif isEven n then helper (n / 2) (count + 1)
+    else helper (3 * n + 1) (count + 1)
+  helper n 0
